316d27d65d6e6fd0f5321de5fdb70720867d07d0
evibhm <evibhm@protonmail.com>
2023-04-26 21:38:05 +0800

    perf: remove debug info

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Search for {}", config.query);
-    println!("In file {}", config.file_path);
+    // println!("Search for {}", config.query);
+    // println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
