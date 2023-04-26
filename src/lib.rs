3fb362a3656c9802b3f0a5a7444d90dd5c8961d5
evibhm <evibhm@protonmail.com>
2023-04-26 21:26:15 +0800

    feat: complete fn search

--- src/lib.rs
+++ src/lib.rs
@@ -17,5 +17,13 @@ Pick three.";
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
