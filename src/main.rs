5f92bb797bab9815a7e73efd571da20add78d946
evibhm <evibhm@protonmail.com>
2023-04-26 10:58:13 +0800

    refacter: use fn parse_config

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Search for {}", query);
     println!("In file {}", file_path);
@@ -13,4 +12,10 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("With text:\n {}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
 }
