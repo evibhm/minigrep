658775a8d6d0a7926409e3bfcf896878e25da28d
evibhm <evibhm@protonmail.com>
2023-04-26 21:45:29 +0800

    test: add case_sensitive and case_insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,37 @@ Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
