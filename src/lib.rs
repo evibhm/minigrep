79681042d1918a42afa3cf369810d3c85e860f4f
evibhm <evibhm@protonmail.com>
2023-04-26 21:25:52 +0800

    test: add fn search test

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
