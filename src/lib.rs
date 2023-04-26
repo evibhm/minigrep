5bc0f77ad701989a8df2c4db5e8414e71f31e7e6
evibhm <evibhm@protonmail.com>
2023-04-26 21:48:37 +0800

    feat: add fn search_case_insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,16 @@ Trust me.";
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
