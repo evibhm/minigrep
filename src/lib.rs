7a353530297317df1bdb93272b45be8c7ad715be
evibhm <evibhm@protonmail.com>
2023-04-26 21:33:44 +0800

    refactor: move everything except fn main to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#![allow(unused)]
+use std::error::Error;
+use std::fs;
 
 #[cfg(test)]
 mod tests {
@@ -26,4 +27,28 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     }
 
     results
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n {}", contents);
+
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
 }
