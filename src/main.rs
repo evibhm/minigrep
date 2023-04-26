0db2e2358185569ce0c456f22d8757fe32217101
evibhm <evibhm@protonmail.com>
2023-04-26 11:20:31 +0800

    feat: handle error in main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -13,14 +14,18 @@ fn main() {
     println!("Search for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
-fn run(config: Config) {
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text:\n {}", contents);
+
+    Ok(())
 }
 
 struct Config {
