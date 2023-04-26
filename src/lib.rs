ed6f8470d2e6fa6b30efffc8fe26e672938f7ee5
evibhm <evibhm@protonmail.com>
2023-04-26 21:40:07 +0800

    feat: complete fn run using fn search

--- src/lib.rs
+++ src/lib.rs
@@ -32,7 +32,9 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n {}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
